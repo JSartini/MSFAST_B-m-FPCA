8788d344f42660c9db44fca930da585af7fcd837
JSartini <jsartin1@jhu.edu>
2026-03-03 18:18:23 -0500

    Adjust penalty calculation and data formatting when setting up MSFAST inputs.

--- MSFAST/MSFAST_Help.R
+++ MSFAST/MSFAST_Help.R
@@ -1,30 +1,18 @@
 # Produce P_alpha using functional basis - numerical integration
-FAST_P <- function(basis, derivs, Q){
-  P0 = matrix(0, ncol = Q, nrow = Q)
+FAST_P <- function(derivs, Q, upp){
   P2 = matrix(0, ncol = Q, nrow = Q)
-  # Adjust desired accuracy as problem becomes more complex due to large # bases
-  if(Q <= 30){
-    des.tol = .Machine$double.eps^0.30
-  }
-  else{
-    des.tol = .Machine$double.eps^0.40
-  }
   for(i in 1:Q){
     for(j in 1:i){
       # Rescale for numerical precision purposes
-      f0 <- function(x){ return(basis[[i]](x) * basis[[j]](x)) } 
       f2 <- function(x){ return(derivs[[i]](x) * derivs[[j]](x)) }
-      P0[i,j] = stats::integrate(f0, lower = 0, upper = 1, subdivisions = 10000, 
-                                 rel.tol = des.tol)$value
-      P2[i,j] = stats::integrate(f2, lower = 0, upper = 1, subdivisions = 10000, 
-                                 rel.tol = des.tol)$value
+      P2[i,j] = stats::integrate(f2, lower = 0, upper = upp,
+                                 subdivisions = 10000)$value
       if(i != j){
-        P0[j,i] = P0[i,j]
         P2[j,i] = P2[i,j]
       }
     }
   } 
-  return(list(P0 = P0, P2 = P2))
+  return(P2)
 }
 
 # Evaluate the orthogonal spline basis at specified time points
@@ -54,19 +42,19 @@ FAST_B <- function(basis_type = "B", Q, Domain){
 }
 
 # Produce inputs for FAST
-MSFAST_datalist <- function(df, N, K, Q, Domain, basis_type = "B",
-                            alpha = 0.1, scale = T){
+MSFAST_datalist <- function(df, N, K, Q, Domain, ord = "Var", basis_type = "B",
+                            alpha = 0.1, scale = T, threads = 1){
   
   # Generate spline basis with derivatives
+  P0 = diag(Q)
   if(basis_type == "B"){
     B_f = OBasis(c(rep(0, 3), seq(0, 1, length.out = Q-2), rep(1, 3)))
     B = evaluate(B_f, Domain)
-    P0 = GramMatrix(B_f)
     P2 = OuterProdSecondDerivative(B_f)
-    P_alpha = alpha * P0 + (1-alpha) * P2
   }
   else{
     B = FAST_B(basis_type, Q, Domain)
+    upp = 1
     if(basis_type == "Fourier"){
       basis = Fourier_bases(Q)
       derivs = Fourier_d2(Q)
@@ -75,6 +63,7 @@ MSFAST_datalist <- function(df, N, K, Q, Domain, basis_type = "B",
       bobj = Splinet_bases(Q)
       basis = bobj$B
       derivs = Splinet_d2(Q, bobj$cInt, bobj$cSlo)
+      upp = 10 # Larger range for stability of numerical integration
     }
     else if(basis_type == "Legendre"){
       basis = Legendre_bases(Q)
@@ -83,9 +72,10 @@ MSFAST_datalist <- function(df, N, K, Q, Domain, basis_type = "B",
     else{
       stop("Basis not supported")
     }
-    P_est = FAST_P(basis, derivs, Q)
-    P_alpha = alpha * P_est$P0 + (1-alpha) * P_est$P2
+    P2 = FAST_P(derivs, Q, upp)
+    P_alpha = alpha * diag(Q) + (1-alpha) * P2
   }
+  P_alpha = alpha * P0 + (1-alpha) * P2
   
   # Scale the Y-values, storing the location and scale
   if(scale){
@@ -100,24 +90,61 @@ MSFAST_datalist <- function(df, N, K, Q, Domain, basis_type = "B",
       summarize(mu_Y = 0, 
                 sd_Y = 1)
   }
-  arranged_df = df %>%
-    arrange(Var, Arg) %>%
-    left_join(const_df, by = "Var") %>%
-    mutate(Y = (Y - mu_Y)/sd_Y) %>%
-    select(-c(mu_Y, sd_Y))  
-  
-  TPC = arranged_df %>%
-    group_by(Var) %>%
-    summarize(m_c = n()) %>%
-    pull(m_c)
-  dim(TPC) = c(P)
-  
   M_val = length(Domain)
   
-  # Format for FAST
-  fast_list = list(N = N, M = M_val, L = nrow(arranged_df), Q = Q, K = K, P = length(TPC), 
-                   Y = arranged_df$Y, Subj = arranged_df$Subj, S = arranged_df$S,
-                   Tp_card = TPC, B = B, P_alpha = P_alpha, consts = const_df)
+  if(ord == "Var"){
+    s_df = data.frame(S_new = 1:length(Domain), 
+                      Arg = sort(Domain))
+    
+    arranged_df = df %>%
+      arrange(Var, Arg) %>%
+      left_join(const_df, by = "Var") %>%
+      left_join(s_df) %>%
+      mutate(Y = (Y - mu_Y)/sd_Y) %>%
+      select(-c(mu_Y, sd_Y))  
+    
+    TPC = arranged_df %>%
+      group_by(Var) %>%
+      summarize(m_c = n()) %>%
+      pull(m_c)
+    dim(TPC) = c(P)
+    
+    # Format for FAST
+    fast_list = list(N = N, M = M_val, L = nrow(arranged_df), Q = Q, K = K, P = length(TPC), 
+                     Y = arranged_df$Y, Subj = arranged_df$Subj, S = arranged_df$S_new,
+                     Tp_card = TPC, B = B, P_alpha = P_alpha, consts = const_df)
+  }
+  else if(ord == "ID"){
+    s_df = data.frame(S_new = 1:length(Domain), 
+                      Arg = sort(Domain))
+    
+    arranged_df = df %>%
+      arrange(Subj, Var, Arg) %>%
+      left_join(const_df, by = "Var") %>%
+      left_join(s_df) %>%
+      mutate(Y = (Y - mu_Y)/sd_Y) %>% #, 
+             # S_new = S_new + (Var - 1)*M_val) %>%
+      select(-c(mu_Y, sd_Y))  
+    
+    IPC = matrix(NA, nrow = N, ncol = P)
+    for(p in 1:P){
+      IPC[,p] = arranged_df %>%
+        filter(Var == p) %>%
+        group_by(Subj) %>%
+        summarize(m_c = n()) %>%
+        pull(m_c)
+    }
+    
+    # Format for FAST
+    fast_list = list(N = N, M = M_val, L = nrow(arranged_df), Q = Q, K = K, 
+                     P = n_distinct(arranged_df$Var), Y = arranged_df$Y, 
+                     S = arranged_df$S_new, ID_card = IPC, B = B, 
+                     P_alpha = P_alpha, consts = const_df)
+    fast_list$Th = threads
+  }
+  else{
+    stop("Ordering for parallelization not supported")
+  }
   return(fast_list)
 }
 
